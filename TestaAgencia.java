9f6146982a96298d8f68175873ae04ebd8737c39
Arthur Oliveira Silva <arthurolsi2003@gmail.com>
2023-08-31 23:17:25 -0300

    Add files via upload

---
+++ TestaAgencia.java
@@ -0,0 +1,15 @@
+
+public class TestaAgencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Agencia ag1 = new Agencia();
+		
+		ag1.nrAgencia = "1";
+		ag1.codBanco = 234;
+		
+		ag1.imprimirDados();
+	}
+
+}
